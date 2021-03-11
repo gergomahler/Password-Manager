84505816277442821dac4c2fd1a3435c39eddb4e
gergomahler <mahlergergo@gmail.com>
2021-03-11 22:32:09 +0100

    password generator shuffle

--- Password Manager/Services/PasswordGenerator.cs
+++ Password Manager/Services/PasswordGenerator.cs
@@ -19,19 +19,40 @@ namespace Password_Manager.Services
 
         private string GeneratePassword(int numOfEachCharType)
         {
+            List<char> pswChars = new List<char>();
             StringBuilder stringBuilder = new StringBuilder();
             for (int i = 0; i < numOfEachCharType; i++)
             {
-                stringBuilder.Append(LOWER_CASE.ToCharArray()[random.Next(LOWER_CASE.Length - 1)]);
-                stringBuilder.Append(UPPER_CAES.ToCharArray()[random.Next(UPPER_CAES.Length - 1)]);
-                stringBuilder.Append(NUMBERS.ToCharArray()[random.Next(NUMBERS.Length - 1)]);
-                stringBuilder.Append(SPECIALS.ToCharArray()[random.Next(SPECIALS.Length - 1)]);
+                pswChars.Add(LOWER_CASE.ToCharArray()[random.Next(LOWER_CASE.Length - 1)]);
+                pswChars.Add(UPPER_CAES.ToCharArray()[random.Next(UPPER_CAES.Length - 1)]);
+                pswChars.Add(NUMBERS.ToCharArray()[random.Next(NUMBERS.Length - 1)]);
+                pswChars.Add(SPECIALS.ToCharArray()[random.Next(SPECIALS.Length - 1)]);
             }
-            char[] selectedChars = stringBuilder.ToString().ToCharArray();
-            string Password = selectedChars.OrderBy(x => random.Next()).ToString();
+
+            Shuffle(pswChars);
+
+            foreach (char c in pswChars)
+            {
+                stringBuilder.Append(c);
+            }
+
+            string Password = stringBuilder.ToString();
 
 
             return Password;
+        }
+
+        public void Shuffle<T>(List<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                int k = (random.Next(0, n) % n);
+                n--;
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
         }
     }
 }
