e114c02f6c888916bc55f28f82513b5a58230545
gergomahler <mahlergergo@gmail.com>
2021-04-01 14:58:49 +0200

    register new user into db

--- Password Manager/Services/DataHandler.cs
+++ Password Manager/Services/DataHandler.cs
@@ -4,17 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Password_Manager.Database;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Password_Manager.Services
 {
     class DataHandler
     {
+        SqlConnection connection = new SqlConnection(Properties.Settings.Default.connection_string);
+        SqlCommand command = new SqlCommand();
+        SqlDataAdapter adapter;
+        SqlDataReader reader;
+
         public void RegisterNewUser (string email, string username, string password)
         {
-            string sqlQuery = "INSERT INTO [User] (Email, Username, Password) " +
+            command.CommandText = "INSERT INTO [User] (Email, Username, Password)" + "VALUES('" + email + "','" + username + "','" + password + "')";
+            command.CommandType = CommandType.Text;
+            command.Connection = connection;
+
+            connection.Open();
+
+            command.ExecuteNonQuery();
+
+            connection.Close();
+
+            /*string sqlQuery = "INSERT INTO [User] (Email, Username, Password) " +
                              "VALUES('" + email + "','" + username + "','" + password + "')";
 
-            DbConnection.ExecuteSQL(sqlQuery);
+            DbConnection.ExecuteSQL(sqlQuery);*/
         }
     }
 }
